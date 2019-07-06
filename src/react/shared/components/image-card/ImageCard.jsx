ef56117bc123415ba5ea73c97c645a1ee7092166
daviddai <david.dai.personal@gmail.com>
2019-07-06 16:27:40 +1000

    add more photo links for top destinations

--- src/react/shared/components/image-card/ImageCard.jsx
+++ src/react/shared/components/image-card/ImageCard.jsx
@@ -7,10 +7,10 @@ import "./image-card.css";
 export const ImageCard = (props) => {
     return (
         <div className="py-2 image-card-container">
-            <div className="image-card-text">
-                <h4>{props.text}</h4>
+            <div className="image-card-text text-center w-100 h-100">
+                <h4 className="font-weight-bolder">{props.text}</h4>
             </div>
-            <img className="card-image" src={props.url} />
+            <img className="card-image rounded" src={props.url} />
         </div>
     );
 };
