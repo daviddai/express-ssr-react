61935255f1062232ded82447c11bb34fa69172cf
ddai <ddai@tripadvisor.com>
2019-07-12 12:04:56 +1000

    removed console log [1m]

--- src/express/api/home-gateway.js
+++ src/express/api/home-gateway.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 export const getHomeData = (callback) => {
-    console.log("getHomeData...");
-
     axios.get("http://localhost:8084/api/gateway/travel-experience/home/data")
         .then((response) => {
             callback(response.data);
         })
-        .catch((err) =>{
+        .catch((err) => {
             console.log(err);
         });
 };
