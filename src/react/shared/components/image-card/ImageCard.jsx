93ad233be951b1b5e274b3d6229938716dfa3505
daviddai <david.dai.personal@gmail.com>
2019-07-13 10:32:24 +1000

    animation for top destination card wip

--- src/react/shared/components/image-card/ImageCard.jsx
+++ src/react/shared/components/image-card/ImageCard.jsx
@@ -7,10 +7,10 @@ import "./image-card.css";
 export const ImageCard = (props) => {
     return (
         <div className="py-2 image-card-container">
-            <div className="image-card-text text-center w-100 h-100">
+            <div className="image-card-text text-center w-100">
                 <h4 className="font-weight-bolder">{props.text}</h4>
             </div>
-            <img className="card-image rounded" src={props.url} />
+            <img className={"card-image rounded " + props.className} src={props.url} />
         </div>
     );
 };
